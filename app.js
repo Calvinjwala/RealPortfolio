d4ba3dc6cf6b0c079194a489ba3f8fcfa0a8ba58
calvinjwala <cjhunjhnuwala@gmail.com>
2015-01-08 00:57:58 -0800

    added bodyparser and methodoverride functions

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var express = require("express"),
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({extended: true}) );
+app.use(methodOverride('_method'));
 
 app.get('/', function(req,res){
     res.render('index');
